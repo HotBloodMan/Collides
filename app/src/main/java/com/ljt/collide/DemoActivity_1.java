5702d779473c38163c89e83cd1e7b95380726715
HotBloodMan <18303011272@163.com>
2017-10-18 15:32:08 +0800

    SlideCollide

---
+++ app/src/main/java/com/ljt/collide/DemoActivity_1.java
@@ -0,0 +1,17 @@
+package com.ljt.collide;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DemoActivity_1 extends AppCompatActivity {
+    private static final String TAG = "DemoActivity_1";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activity_1);
+        initView();
+    }
+    private void initView() {
+
+    }
+}
